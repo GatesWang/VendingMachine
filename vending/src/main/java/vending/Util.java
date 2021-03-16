602ee244b8d5b15abc340975bccd45d40d00ba6c
= <gateswang00@gmail.com>
2021-03-15 21:18:07 -0400

    Added comments for Util functions.

--- vending/src/main/java/vending/Util.java
+++ vending/src/main/java/vending/Util.java
@@ -3,6 +3,20 @@ package vending;
 import java.util.ArrayList;
 
 public class Util{
+    /*
+        This simulates clearing screen. It works in IDE's too. 
+    */
+    public static void clearScreen(){
+        for(int i=0; i<5; i++){
+            System.out.println();
+        }
+    }
+    /*
+        Converts int to respective character
+        0 -> a
+        1 -> b
+        etc
+    */
     public static String getCharForNumber(int i) throws Exception{
         if(i>0 && i<27){
             return String.valueOf((char)(i + 64));
@@ -11,7 +25,14 @@ public class Util{
             throw new Exception("That number can't be mapped to a letter");
         }
     }
-
+    /*
+        Given the number of rows and columns
+        generate the potential spots in a vending machine e.g.
+        getSpots(2,2) 
+        => 
+        A1 A2
+        B1 B2
+    */
     public static ArrayList<String> getSpots(int rows, int cols) throws Exception{
         ArrayList<String> spots = new ArrayList<>();
         for(int i=1; i<=rows; i++){
